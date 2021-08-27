114c9f731f89e38543979bd3667a7b88f5d19244
Favio Náquira <fnaquira@conflux-aqp.com>
2021-08-27 18:07:20 -0500

    [IMP] Consulta AFP

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,23 +1,33 @@
-const rucConflux = require("../index");
+const rucPeru = require('../index');
 
-test("should fail with RUC 20406494171 ", async () => {
-    const ruc = new rucConflux();
-    await expect(ruc.ruc("20406494171")).rejects.toThrow("Request failed with status code 500");
+test('should fail with RUC 20406494171', async () => {
+	const ruc = new rucPeru();
+	await expect(ruc.ruc('20406494171')).rejects.toThrow(
+		'Request failed with status code 500'
+	);
 });
 
-test("should get RUC 20406494170 ", async () => {
-    const ruc = new rucConflux();
-    await expect(ruc.ruc("20406494170")).toBeInstanceOf(Object);
+test('should get RUC 20406494170', async () => {
+	const ruc = new rucPeru();
+	await expect(ruc.ruc('20406494170')).toBeInstanceOf(Object);
 });
 
-test("should get RUC 20406494170 ", async () => {
-    const ruc = new rucConflux();
-    const data = await ruc.ruc("20406494170");
-    expect(data.nombre).toEqual("CONFLUX MULTISERVICIOS SOCIEDAD COMERCIAL DE RESPONSABILIDAD LIMITADA");
+test('should get RUC 20406494170', async () => {
+	const ruc = new rucPeru();
+	const data = await ruc.ruc('20406494170');
+	expect(data.nombre).toEqual(
+		'CONFLUX MULTISERVICIOS SOCIEDAD COMERCIAL DE RESPONSABILIDAD LIMITADA'
+	);
 });
 
-test("should get DNI 70479264 ", async () => {
-    const ruc = new rucConflux();
-    const data = await ruc.dni("70479264");
-    expect(data.name).toEqual("MONTESINOS ARAMAYO JORGE OSMAR");
+test('should get DNI 70479264', async () => {
+	const ruc = new rucPeru();
+	const data = await ruc.dni('70479264');
+	expect(data.name).toEqual('MONTESINOS ARAMAYO JORGE OSMAR');
+});
+
+test('should get AFP', async () => {
+	const ruc = new rucPeru();
+	const data = await ruc.afp();
+	expect(data[0].name).toEqual('HABITAT');
 });
--- index.js
+++ index.js
@@ -1,60 +1,88 @@
-const axios = require("axios");
-const verifyRuc = require("ruc-validate");
+const axios = require('axios');
+const verifyRuc = require('ruc-validate');
 
-const URL_RUC = "https://ruc.conflux.pe/ruc";
-const URL_DNI = "https://ruc.conflux.pe/dni";
+const URL_RUC = 'https://ruc.conflux.pe/ruc';
+const URL_DNI = 'https://ruc.conflux.pe/dni';
+const URL_AFP = 'https://ruc.conflux.pe/afp';
 
-class RucConflux {
-    constructor(token) {
-        if (token === undefined) {
-            token = process.env.RUC_TOKEN;
-        }
-        this.token = token;
-    }
-    /**
-     * Método para obtener RUC
-     * @method
-     * @param {String} documento
-     * @returns {Promise}
-     */
-    ruc(documento) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                if (verifyRuc) {
-                    const headers = {
-                        Authorization: this.token,
-                    };
-                    const { data } = await axios.get(`${URL_RUC}/${documento}`, { headers });
-                    resolve(data);
-                } else {
-                    reject("RUC inválido, verifique por favor.");
-                }
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
-    /**
-     * Método para obtener DNI
-     * @method
-     * @param {String} documento
-     * @returns {Promise}
-     */
-    dni(documento) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const headers = {
-                    Authorization: this.token,
-                };
-                const { data } = await axios.get(`${URL_DNI}/${documento}`, { headers });
-                resolve(data);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
+class RucPeru {
+	constructor(token) {
+		if (token === undefined) {
+			token = process.env.RUC_TOKEN;
+		}
+		this.token = token;
+	}
+	/**
+	 * Método para obtener RUC
+	 * @method
+	 * @param {String} documento
+	 * @returns {Promise}
+	 */
+	ruc(documento) {
+		return new Promise(async (resolve, reject) => {
+			if (this.token === undefined) reject('No hay un token establecido.');
+			try {
+				if (verifyRuc) {
+					const headers = {
+						Authorization: this.token,
+					};
+					const { data } = await axios.get(`${URL_RUC}/${documento}`, {
+						headers,
+					});
+					resolve(data);
+				} else {
+					reject('RUC inválido, verifique por favor.');
+				}
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+	/**
+	 * Método para obtener DNI
+	 * @method
+	 * @param {String} documento
+	 * @returns {Promise}
+	 */
+	dni(documento) {
+		return new Promise(async (resolve, reject) => {
+			if (this.token === undefined) reject('No hay un token establecido.');
+			try {
+				const headers = {
+					Authorization: this.token,
+				};
+				const { data } = await axios.get(`${URL_DNI}/${documento}`, {
+					headers,
+				});
+				resolve(data);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+	/**
+	 * Método para obtener AFP
+	 * @method
+	 * @returns {Promise}
+	 */
+	afp() {
+		return new Promise(async (resolve, reject) => {
+			if (this.token === undefined) reject('No hay un token establecido.');
+			try {
+				const headers = {
+					Authorization: this.token,
+				};
+				const { data } = await axios.get(`${URL_AFP}`, {
+					headers,
+				});
+				resolve(data);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
 }
 
 module.exports = function (token) {
-    return new RucConflux(token);
+	return new RucPeru(token);
 };
